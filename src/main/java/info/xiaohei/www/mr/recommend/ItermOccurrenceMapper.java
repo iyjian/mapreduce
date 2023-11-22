9e0e882361dcdc113c27ca30197c4996bf2670ae
w.c <wc@jcjy.com>
2023-11-22 18:08:47 +0800

    fix

--- src/main/java/info/xiaohei/www/mr/recommend/ItermOccurrenceMapper.java
+++ src/main/java/info/xiaohei/www/mr/recommend/ItermOccurrenceMapper.java
@@ -5,8 +5,6 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.util.ToolRunner;
-import org.apache.hadoop.conf.Configuration;
 import java.io.IOException;
 
 /**
