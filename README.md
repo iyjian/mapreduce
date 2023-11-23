e964a9f8e060fb7d7595307215c9bcb20752df5e
w.c <wc@jcjy.com>
2023-11-23 17:57:14 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 cp target/bigdata-mr-1.0-SNAPSHOT.jar /home/wucho/projects/softwares/spark-hadoop-docker/share
+
+hadoop fs -rm -r /javastep1
+hadoop jar bigdata-mr-1.0-SNAPSHOT.jar info.demo.www.mr.recommend.ItermOccurrence /ratings_teaching /javastep1
+hadoop fs -cat /javastep1/part-r-00000
+
+
+hadoop fs -rm -r /DistributedCacheTest
+hadoop jar bigdata-mr-1.0-SNAPSHOT.jar info.demo.www.mr.recommend.DistributedCacheTest /ratings_teaching /DistributedCacheTest
+hadoop fs -cat /DistributedCacheTest/part-r-00000
--- src/main/java/info/demo/www/mr/recommend/DistributedCacheTest.java
+++ src/main/java/info/demo/www/mr/recommend/DistributedCacheTest.java
@@ -1,5 +1,7 @@
 package info.demo.www.mr.recommend;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,8 +15,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.net.URI;
 
-public class ItermOccurrence {
+public class DistributedCacheTest {
 
   public static class Map
       extends Mapper<Object, Text, Text, Text> {
@@ -23,37 +26,45 @@ public class ItermOccurrence {
     private Text outputVal = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      String[] cols = value.toString().split(",");
-      outputKey.set(cols[0]);
-      outputVal.set(cols[1] + ":" + cols[2]);
-      context.write(outputKey, outputVal);
+      // String[] cols = value.toString().split(",");
+      // outputKey.set(cols[0]);
+      // outputVal.set(cols[1] + ":" + cols[2]);
+      // context.write(outputKey, outputVal);
+
+      BufferedReader bufferedReader = new BufferedReader(new FileReader("cacheSample1"));
+      
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        String[] cols = line.split(",");
+        outputKey.set(cols[0]);
+        outputVal.set(cols[1] + ":" + cols[2]);
+        context.write(outputKey, outputVal);        
+      }
+
+      bufferedReader.close();
+
     }
   }
 
   public static class Reduce
       extends Reducer<Text, Text, Text, Text> {
     
-    private Text outputVal = new Text();
-
     public void reduce(Text key, Iterable<Text> values,
         Context context) throws IOException, InterruptedException {
-      List<String> valueList = new ArrayList<>();
-      
-      for (Text value : values) {
-        valueList.add(value.toString());
-      }
 
-      Collections.sort(valueList);
-
-      outputVal.set(String.join(",", valueList));
-      
-      context.write(key, outputVal);
+      context.write(key, key);
+    
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "ItemOccurenceStep1");
+    Job job = Job.getInstance(conf, "DistributedCacheTest");
+
+
+    job.addCacheFile(new URI("/rattings_middle/ratings_middle.csv#cacheSample1"));
+
+
     job.setJarByClass(ItermOccurrence.class);
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
